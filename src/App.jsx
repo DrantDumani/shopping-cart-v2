07acc33b9b9807f7cdefefff1da55e4b9b9dc446
DrantDumani <darnjames@gmail.com>
2024-03-19 00:09:31 -0400

    Create skeleton for root page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
+import { Outlet } from "react-router";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <>
+      <header className="header">
+        <Navbar />
+      </header>
+
+      <main className="main">
+        <Outlet />
+      </main>
+
+      <footer className="footer">
+        <p>
+          Created by Darnell. <a href="#">Github repo</a>
+        </p>
+        <p>Font and homepage image are property of Nintendo.</p>
+        <p>
+          Built using React Router and{" "}
+          <a href="https://fakestoreapi.com/">Fakestore API</a>
+        </p>
+      </footer>
+    </>
+  );
 }
 
 export default App;
