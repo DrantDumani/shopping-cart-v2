9aaf9d499d902cc69ea1f64eec6166ad3d344bf1
DrantDumani <darnjames@gmail.com>
2024-03-21 14:35:45 -0400

    Create mobile navbar style

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,39 @@
 import { Link, NavLink } from "react-router-dom";
 import PropTypes from "prop-types";
+import styles from "./Navbar.module.css";
 
 function Navbar({ amount }) {
   return (
-    <nav className="navBar">
-      <ul className="navList">
-        <li>
-          <Link to="/">Shellendorf Antiques</Link>
+    <nav className={styles.nav}>
+      <ul className={styles.list}>
+        <li className={styles.listItemBig}>
+          <Link className={styles.linkBig} to="/">
+            Shellendorf Antiques
+          </Link>
         </li>
-        <li>
-          <NavLink to="/shop">Shop</NavLink>
-        </li>
-        <li>
-          <NavLink to="/cart">Cart: {amount}</NavLink>
+        <li className={styles.listItemSmall}>
+          <ul className={styles.nestedList}>
+            <li className={styles.nestedListItem}>
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? styles.active : styles.navLink
+                }
+                to="/shop"
+              >
+                Shop
+              </NavLink>
+            </li>
+            <li className={styles.nestedListItem}>
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? styles.active : styles.navLink
+                }
+                to="/cart"
+              >
+                Cart: {amount}
+              </NavLink>
+            </li>
+          </ul>
         </li>
       </ul>
     </nav>
